267ce37098a7d2d52483b5fe8f6442a15fc27539
heyset <c.inaciomatheus@gmail.com>
2021-11-29 02:54:19 -0300

    Adds error handling

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -22,12 +23,27 @@ export class HeroService {
   }
 
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError<Hero[]>('getHeroes', [])),
+      );
   }
 
   getHero(id: number): Observable<Hero> {
     const hero = HEROES.find((h) => h.id === id)!;
     this.log(`HeroService: fetched hero id=${id}`);
     return of(hero);
+  }
+
+  handleError<T>(operation = 'operation', result?: T) {
+    return (error: any) : Observable<T> => {
+      // TODO send error to remote logging
+      console.error(error);
+
+      // TODO better transform the error to be understandable by the user
+      this.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
   }
 }
