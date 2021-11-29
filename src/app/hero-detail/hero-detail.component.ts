f58bf205a96db4bf4584f2247d000af748e7f4f7
heyset <c.inaciomatheus@gmail.com>
2021-11-29 02:32:04 -0300

    Adds service to fetch individual hero

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,9 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit, Input } from '@angular/core';
+import { Location } from '@angular/common';
 
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -10,9 +13,20 @@ import { Hero } from '../hero';
 export class HeroDetailComponent implements OnInit {
   @Input() hero?: Hero;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location,
+  ) { }
 
   ngOnInit(): void {
+    this.fetchHero();
+  }
+
+  fetchHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe((hero) => this.hero = hero);
   }
 
 }
