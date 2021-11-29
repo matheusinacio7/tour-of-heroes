870578e644f0a2d113cfa3b49fc2affc2ffb461b
heyset <c.inaciomatheus@gmail.com>
2021-11-29 00:27:46 -0300

    Uses message service in hero service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,17 +1,22 @@
+import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Injectable } from '@angular/core';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(
+    private messageService: MessageService,
+  ) { }
 
   getHeroes() : Observable<Hero[]> {
-    return of(HEROES);
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
   }
 }
