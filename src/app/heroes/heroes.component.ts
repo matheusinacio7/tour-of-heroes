ff9a4766edc8fd3969428dfff46db115a8ea4c53
heyset <c.inaciomatheus@gmail.com>
2021-11-29 00:39:47 -0300

    Adds hero selection message

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { HeroService } from '../hero.service';
 import type { Hero } from '../hero';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,7 +15,8 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
   constructor(
-    private heroService : HeroService,
+    private heroService: HeroService,
+    private messageService: MessageService,
   ) { }
 
   ngOnInit(): void {
@@ -28,5 +30,6 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
